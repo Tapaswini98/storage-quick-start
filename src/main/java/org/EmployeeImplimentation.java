110bca2fd56af2c2555fe245ba6f252261cd08b9
Tapaswini kar <kar.tapaswini1111@gmail.com>
2020-12-28 14:22:34 +0530

    Employee Interface added

---
+++ src/main/java/org/EmployeeImplimentation.java
@@ -0,0 +1,18 @@
+package org;
+
+public class EmployeeImplimentation implements EmployeeInterface {
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+}
---
+++ src/main/java/org/EmployeeInterface.java
@@ -0,0 +1,7 @@
+package org;
+
+public interface EmployeeInterface {
+    public abstract void create();
+    public abstract void update();
+    public abstract void delete();
+}
